290c960942bb186c973c8c8d02d6778974dc49ee
ADMIN\Admin <hnmnhnmn2@gmail.com>
2022-04-26 08:49:59 +0700

    26/4

--- script.js
+++ script.js
@@ -4,56 +4,261 @@ let active = false;
 
 // First we'll have to set up our event listeners
 // We want to watch for clicks on our scroller
-document.querySelector('.scroller').addEventListener('mousedown',function(){
+document.querySelector(".scroller").addEventListener("mousedown", function () {
   active = true;
   // Add our scrolling class so the scroller has full opacity while active
-  document.querySelector('.scroller').classList.add('scrolling');
+  document.querySelector(".scroller").classList.add("scrolling");
 });
 // We also want to watch the body for changes to the state,
 // like moving around and releasing the click
 // so let's set up our event listeners
-document.body.addEventListener('mouseup',function(){
+document.body.addEventListener("mouseup", function () {
   active = false;
-  document.querySelector('.scroller').classList.remove('scrolling');
+  document.querySelector(".scroller").classList.remove("scrolling");
 });
-document.body.addEventListener('mouseleave',function(){
+document.body.addEventListener("mouseleave", function () {
   active = false;
-  document.querySelector('.scroller').classList.remove('scrolling');
+  document.querySelector(".scroller").classList.remove("scrolling");
 });
 
 // Let's figure out where their mouse is at
-document.body.addEventListener('mousemove',function(e){
+document.body.addEventListener("mousemove", function (e) {
   if (!active) return;
   // Their mouse is here...
   let x = e.pageX;
   // but we want it relative to our wrapper
-  x -= document.querySelector('.wrapper').getBoundingClientRect().left;
+  x -= document.querySelector(".wrapper").getBoundingClientRect().left;
   // Okay let's change our state
   scrollIt(x);
 });
 
 // Let's use this function
-function scrollIt(x){
-    let transform = Math.max(0,(Math.min(x,document.querySelector('.wrapper').offsetWidth)));
-    document.querySelector('.after').style.width = transform+"px";
-    document.querySelector('.scroller').style.left = transform-25+"px";
+function scrollIt(x) {
+  let transform = Math.max(
+    0,
+    Math.min(x, document.querySelector(".wrapper").offsetWidth)
+  );
+  document.querySelector(".after").style.width = transform + "px";
+  document.querySelector(".scroller").style.left = transform - 25 + "px";
 }
 
-// Let's set our opening state based off the width, 
+// Let's set our opening state based off the width,
 // we want to show a bit of both images so the user can see what's going on
 scrollIt(150);
 
 // And finally let's repeat the process for touch events
 // first our middle scroller...
-document.querySelector('.scroller').addEventListener('touchstart',function(){
+document.querySelector(".scroller").addEventListener("touchstart", function () {
   active = true;
-  document.querySelector('.scroller').classList.add('scrolling');
+  document.querySelector(".scroller").classList.add("scrolling");
 });
-document.body.addEventListener('touchend',function(){
+document.body.addEventListener("touchend", function () {
   active = false;
-  document.querySelector('.scroller').classList.remove('scrolling');
+  document.querySelector(".scroller").classList.remove("scrolling");
 });
-document.body.addEventListener('touchcancel',function(){
+document.body.addEventListener("touchcancel", function () {
   active = false;
-  document.querySelector('.scroller').classList.remove('scrolling');
+  document.querySelector(".scroller").classList.remove("scrolling");
 });
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+//
+// carrousel
+//
+const slideData = [
+  {
+    index: 0,
+    headline: "New Fashion Apparel",
+    button: "Shop now",
+    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg",
+  },
+
+  {
+    index: 1,
+    headline: "In The Wilderness",
+    button: "Book travel",
+    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg",
+  },
+
+  {
+    index: 2,
+    headline: "For Your Current Mood",
+    button: "Listen",
+    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg",
+  },
+
+  {
+    index: 3,
+    headline: "Focus On The Writing",
+    button: "Get Focused",
+    src: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg",
+  },
+];
+
+// =========================
+// Slide
+// =========================
+
+class Slide extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.handleSlideClick = this.handleSlideClick.bind(this);
+    this.imageLoaded = this.imageLoaded.bind(this);
+    this.slide = React.createRef();
+  }
+
+  handleMouseMove(event) {
+    const el = this.slide.current;
+    const r = el.getBoundingClientRect();
+
+    el.style.setProperty(
+      "--x",
+      event.clientX - (r.left + Math.floor(r.width / 2))
+    );
+    el.style.setProperty(
+      "--y",
+      event.clientY - (r.top + Math.floor(r.height / 2))
+    );
+  }
+
+  handleMouseLeave(event) {
+    this.slide.current.style.setProperty("--x", 0);
+    this.slide.current.style.setProperty("--y", 0);
+  }
+
+  handleSlideClick(event) {
+    this.props.handleSlideClick(this.props.slide.index);
+  }
+
+  imageLoaded(event) {
+    event.target.style.opacity = 1;
+  }
+
+  render() {
+    const { src, button, headline, index } = this.props.slide;
+    const current = this.props.current;
+    let classNames = "slide";
+
+    if (current === index) classNames += " slide--current";
+    else if (current - 1 === index) classNames += " slide--previous";
+    else if (current + 1 === index) classNames += " slide--next";
+
+    return /*#__PURE__*/ React.createElement(
+      "li",
+      {
+        ref: this.slide,
+        className: classNames,
+        onClick: this.handleSlideClick,
+        onMouseMove: this.handleMouseMove,
+        onMouseLeave: this.handleMouseLeave,
+      } /*#__PURE__*/,
+
+      React.createElement(
+        "div",
+        { className: "slide__image-wrapper" } /*#__PURE__*/,
+        React.createElement("img", {
+          className: "slide__image",
+          alt: headline,
+          src: src,
+          onLoad: this.imageLoaded,
+        })
+      ) /*#__PURE__*/,
+
+      React.createElement(
+        "article",
+        { className: "slide__content" } /*#__PURE__*/,
+        React.createElement(
+          "h2",
+          { className: "slide__headline" },
+          headline
+        ) /*#__PURE__*/,
+        React.createElement(
+          "button",
+          { className: "slide__action btn" },
+          button
+        )
+      )
+    );
+  }
+}
+
+// =========================
+// Slider control
+// =========================
+
+const SliderControl = ({ type, title, handleClick }) => {
+  return /*#__PURE__*/ React.createElement(
+    "button",
+    {
+      className: `btn btn--${type}`,
+      title: title,
+      onClick: handleClick,
+    } /*#__PURE__*/,
+    React.createElement(
+      "svg",
+      { className: "icon", viewBox: "0 0 24 24" } /*#__PURE__*/,
+      React.createElement("path", {
+        d: "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z",
+      })
+    )
+  );
+};
+
+// =========================
+// Slider
+// =========================
+// scroll
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: "smooth",
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: "smooth",
+});
+
+// Scroll to a certain element
+document.querySelector(".hello").scrollIntoView({
+  behavior: "smooth",
+});
+
+// email function starts
+function sendEmail() {
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "hnmnhnmnemail@gmail.com",
+    Password :  "Hnmnhnmn2",
+    To : 'hnmhnmn2@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "Contact form from Remove Background app",
+    Body : "And this is the body",
+  }).then((message) => alert(message));
+}
+
+// email function ends
